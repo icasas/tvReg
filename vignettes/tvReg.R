2ea9f4cce9a1408efd3863d42ddb29273f81d668
Rubén Fernández-Casal <rubenfcasal@gmail.com>
2021-11-12 13:31:14 +0100

    Corrected YAML

--- vignettes/tvReg.R
+++ vignettes/tvReg.R
@@ -1,5 +1,5 @@
 ## ----setup, include = FALSE---------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE, tidy = TRUE, fig.width = 6,
+knitr::opts_chunk$set(echo = TRUE, fig.width = 6,
                       fig.height = 4, fig.align = "center")
 library(tvReg)
 
